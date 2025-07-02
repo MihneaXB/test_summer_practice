098b30ec2906b4adbf47f96c018011fca0cd415e
MihneaXB <galusca.mihnea@yahoo.com>
2025-07-02 11:58:44 +0300

    polishing the code

--- src/main/java/com/createq/testsummerpractice/service/impl/DefaultSchoolService.java
+++ src/main/java/com/createq/testsummerpractice/service/impl/DefaultSchoolService.java
@@ -3,9 +3,12 @@ package com.createq.testsummerpractice.service.impl;
 import com.createq.testsummerpractice.model.SchoolModel;
 import com.createq.testsummerpractice.repository.SchoolRepository;
 import com.createq.testsummerpractice.service.SchoolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class DefaultSchoolService implements SchoolService {
     private final SchoolRepository schoolRepository;
 
@@ -13,6 +16,7 @@ public class DefaultSchoolService implements SchoolService {
         return schoolRepository;
     }
 
+    @Autowired
     public  DefaultSchoolService(SchoolRepository schoolRepository) {
         this.schoolRepository = schoolRepository;
     }
